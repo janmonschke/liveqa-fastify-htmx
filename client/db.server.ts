b05fe73b723155bffb20216f8ec54ee2f29843a3
Jan Monschke <janmonschke@fastmail.com>
2025-03-14 17:07:20 +0100

    log query durations

--- client/db.server.ts
+++ client/db.server.ts
@@ -1,4 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 import { remember } from "@epic-web/remember";
 
-export const db = remember("prisma", () => new PrismaClient());
+export const db = remember(
+  "prisma",
+  () =>
+    new PrismaClient({
+      log: [
+        {
+          emit: "event",
+          level: "query",
+        },
+        {
+          emit: "stdout",
+          level: "error",
+        },
+        {
+          emit: "stdout",
+          level: "info",
+        },
+        {
+          emit: "stdout",
+          level: "warn",
+        },
+      ],
+    })
+);
+
+db.$on("query", (e) => {
+  console.log("Duration: " + e.duration + "ms");
+});

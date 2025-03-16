19ca2d5e9735d70c226cbd86fa9f662de63ea981
Jan Monschke <janmonschke@fastmail.com>
2025-03-16 13:09:08 +0100

    fix output

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,13 @@ import viteFastifyHtmx from "@fastify/htmx/plugin";
 export default {
   root: join(dirname(fileURLToPath(import.meta.url)), "client"),
   plugins: [viteFastifyHtmx()],
+  build: {
+    rollupOptions: {
+      // Without these options, too many modules, including
+      // server modules were included in the build output .
+      output: { preserveModules: true, preserveModulesRoot: "client" },
+      preserveEntrySignatures: "strict",
+      // </output-hack>
+    },
+  },
 };

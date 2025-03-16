4562a99323e608b689ff24210f0d37891075f823
Jan Monschke <janmonschke@fastmail.com>
2025-03-16 20:33:33 +0100

    add icon

--- client/index.jsx
+++ client/index.jsx
@@ -14,7 +14,10 @@ export { default as root } from "/:root.jsx";
 // Can also be a function that receives ({ app, req, reply })
 export const head = (
   <>
-    <title>Default title</title>
+    <link
+      rel="icon"
+      href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🙋‍♀️</text></svg>"
+    ></link>
     <meta name="htmx-config" content='{"scrollIntoViewOnBoost":false}'></meta>
   </>
 );
